1a0d6ba3256a2dfcbc14204a3bccea14daeeb783
omahajanbe24-cmd <omahajan_be24@thapar.edu>
2026-04-26 15:48:53 +0530

    Add files via upload

---
+++ Assignment8/processes.cpp
@@ -0,0 +1,23 @@
+/*Ojaswin Mahajan - 1024150083*/
+
+#include <iostream>
+using namespace std;
+
+template<typename T>
+void process(T a){
+    cout << "Only 1 parameter of a type" << endl;
+}
+template<typename T>
+void process(T a, T b){
+    cout << "2 parameters of same type" << endl;
+}
+template<typename T, typename U>
+void process(T a, U b){
+    cout << "2 parameters of different type" << endl;
+}
+int main(){
+    process(2);
+    process(2, 2);
+    process(2.0, 2.0f);
+    return 0;
+}
